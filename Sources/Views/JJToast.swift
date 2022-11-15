4709de97f426302b64def32455d0587670967dba
Jero <wjzhlo@163.com>
2022-11-15 16:30:08 +0800

    添加加载动画

--- Sources/Views/JJToast.swift
+++ Sources/Views/JJToast.swift
@@ -12,6 +12,7 @@ public class JJToast {
     ///  单利
     static let share = JJToast()
     private init() { }
+    private let activityTag = 1999
     
     /// 父视图底色
     var bottomColor = UIColor.clear
@@ -29,56 +30,31 @@ public class JJToast {
     var minShowTime = 1.0
     /// 最大显示时长
     var maxShowTime = 5.0
+    /// 禁用背景的事件响应。若为 false ，则显示在 Toast 背景下的控件会响应事件传递。例：按钮允许点击、列表允许滑动
+    var disableBackgroundResponder = true
 }
 
-extension JJToast {
+public extension JJToast {
     
     /// 显示Toast
     /// - Parameters:
     ///   - text: 显示的文本
-    ///   - targetView: 显示在哪个视图上。若不传默认是 window
-    public class func show(_ text: String?, on targetView: UIView? = nil) {
+    ///   - toView: 显示在哪个视图上。若不传默认是 window
+    class func show(_ text: String?, to toView: UIView? = nil) {
         
         guard let text, text.isEmpty == false else { return }
-
+        
         DispatchQueue.main.async {
-                        
-            var parentView = targetView
-            if parentView == nil {
-                parentView = UIApplication.shared.windows.filter({ $0.isKeyWindow }).last
-            }
-            guard let parentView else { return }
             
-            let toastBgView = UIView()
-            toastBgView.backgroundColor = JJToast.share.bottomColor
-            parentView.addSubview(toastBgView)
+            var bgView = toView
+            if bgView == nil { bgView = getWindow() }
+            guard let bgView else { return }
             
-            let toastView = UIView()
-            toastView.backgroundColor = JJToast.share.bgColor
-            toastView.layer.cornerRadius = JJToast.share.bgCornerRadius
-            toastView.layer.masksToBounds = true
-            toastBgView.addSubview(toastView)
-
-            let imageView = UIImageView()
-            imageView.image = JJToast.share.bgImage
-            imageView.backgroundColor = UIColor.clear
-            toastView.addSubview(imageView)
-            
-            let textLabel = UILabel()
-            textLabel.numberOfLines = 0
-            toastView.addSubview(textLabel)
-            
-            let pStyle = NSMutableParagraphStyle()
-            pStyle.lineSpacing = 5.0
-            
-            let mStr = NSMutableAttributedString(string: text)
-            mStr.addAttribute(.paragraphStyle, value: pStyle, range: NSMakeRange(0, mStr.length))
-            mStr.addAttribute(.font, value: JJToast.share.textFont, range: NSMakeRange(0, mStr.length))
-            mStr.addAttribute(.foregroundColor, value: JJToast.share.textColor, range: NSMakeRange(0, mStr.length))
-            textLabel.attributedText = mStr
-            
+            let (toastBgView, toastView, imageView, textLabel) = createToast(toView: bgView, text: text)
             let parentSize = toastBgView.superview?.size ?? UIScreen.main.bounds.size
-            let textSize = mStr.boundingRect(with: CGSize(width: parentSize.width - 80.0, height: 0), options: .usesLineFragmentOrigin, context: nil).size
+            let textSize = textLabel.attributedText!.boundingRect(with: CGSize(width: parentSize.width - 80.0, height: 0),
+                                                                  options: .usesLineFragmentOrigin,
+                                                                  context: nil).size
             let toastSize = CGSize(width: textSize.width + 30.0, height: textSize.height + 30.0)
             
             toastBgView.frame = CGRect(origin: CGPoint.zero, size: parentSize)
@@ -120,5 +96,127 @@ extension JJToast {
                 }
             }
         }
+    }
+    
+    
+    /// 显示加载动画
+    /// - Parameters:
+    ///   - toView: 在哪个视图上显示
+    ///   - text: 提示的文本
+    class func showActivity(to toView: UIView? = nil, text: String? = nil) {
+        
+        DispatchQueue.main.async {
+            
+            var bgView = toView
+            if bgView == nil { bgView = getWindow() }
+            guard let bgView else { return }
+            
+            let (toastBgView, toastView, imageView, textLabel) = createToast(toView: bgView, text: text ?? "")
+            let parentSize = toastBgView.superview?.size ?? UIScreen.main.bounds.size
+            toastBgView.frame = CGRect(origin: CGPoint.zero, size: parentSize)
+            toastBgView.tag = JJToast.share.activityTag
+            
+            let activitySize = CGSizeMake(105, 105)
+            let activityView = UIActivityIndicatorView(frame: CGRect(origin: CGPoint.zero, size: activitySize))
+            activityView.backgroundColor = UIColor.clear
+            activityView.style = .large
+            activityView.color = UIColor.white
+            activityView.startAnimating()
+            toastView.addSubview(activityView)
+            
+            if let text, text.isEmpty == false {
+                let textSize = textLabel.attributedText!.boundingRect(with: CGSize(width: parentSize.width - 80.0, height: 0),
+                                                                      options: .usesLineFragmentOrigin,
+                                                                      context: nil).size
+                let toastSize = CGSize(width: max(textSize.width + 30.0, activitySize.width), height: textSize.height + activitySize.height)
+                toastView.frame = CGRect(origin: CGPoint.zero, size: toastSize)
+                imageView.frame = CGRect(origin: CGPoint.zero, size: toastSize)
+                textLabel.frame = CGRect(origin: CGPoint.zero, size: textSize)
+
+                toastView.center = CGPoint(x: toastBgView.width/2.0, y: toastBgView.height/2.0)
+                imageView.center = CGPoint(x: toastView.width/2.0, y: toastView.height/2.0)
+                textLabel.center = CGPoint(x: toastView.width/2.0, y: toastView.height/2.0 + 30)
+                activityView.center = CGPoint(x: toastView.width/2.0, y: activitySize.height/2.0)
+            } else {
+                toastView.frame = CGRect(origin: CGPoint.zero, size: activitySize)
+                imageView.frame = CGRect(origin: CGPoint.zero, size: activitySize)
+                textLabel.frame = CGRect(origin: CGPoint.zero, size: activitySize)
+                
+                toastView.center = CGPoint(x: toastBgView.width/2.0, y: toastBgView.height/2.0)
+            }
+            
+            toastBgView.alpha = 0
+            UIView.animate(withDuration: 0.2) {
+                toastBgView.alpha = 1
+            } completion: { _ in
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    hideActivity(from: toView)
+                }
+            }
+        }
+    }
+    
+    /// 隐藏加载动画
+    /// - Parameter fromView: 从哪个视图上移除
+    class func hideActivity(from fromView: UIView? = nil) {
+        
+        var bgView = fromView
+        if bgView == nil { bgView = getWindow() }
+        guard let bgView else { return }
+        guard let toastBgView = bgView.viewWithTag(JJToast.share.activityTag) else { return }
+        
+        UIView.animate(withDuration: 0.2) {
+            toastBgView.alpha = 1
+        } completion: { _ in
+            toastBgView.removeFromSuperview()
+        }
+    }
+}
+
+private extension JJToast {
+    
+    /// 获取 window 视图
+    class func getWindow() -> UIView? {
+        return UIApplication.shared.windows.filter({ $0.isKeyWindow }).last
+    }
+    
+    /// 创建 Toast 基本视图组成
+    /// - Parameters:
+    ///   - toView: 显示在哪个 view 上
+    ///   - text: 显示的文本
+    /// - Returns: 依次返回：toastBgView, toastView, imageView, textLabel
+    class func createToast(toView: UIView, text: String) -> (UIView, UIView, UIImageView, UILabel) {
+        
+        let toastBgView = UIView()
+        toastBgView.backgroundColor = JJToast.share.bottomColor
+        toastBgView.isUserInteractionEnabled = JJToast.share.disableBackgroundResponder
+        toView.addSubview(toastBgView)
+        
+        let toastView = UIView()
+        toastView.backgroundColor = JJToast.share.bgColor
+        toastView.layer.cornerRadius = JJToast.share.bgCornerRadius
+        toastView.layer.masksToBounds = true
+        toastBgView.addSubview(toastView)
+
+        let imageView = UIImageView()
+        imageView.backgroundColor = UIColor.clear
+        imageView.image = JJToast.share.bgImage
+        toastView.addSubview(imageView)
+        
+        let textLabel = UILabel()
+        textLabel.backgroundColor = UIColor.clear
+        textLabel.numberOfLines = 0
+        toastView.addSubview(textLabel)
+        
+        let pStyle = NSMutableParagraphStyle()
+        pStyle.lineSpacing = 5.0
+        
+        let mStr = NSMutableAttributedString(string: text)
+        mStr.addAttribute(.paragraphStyle, value: pStyle, range: NSMakeRange(0, mStr.length))
+        mStr.addAttribute(.font, value: JJToast.share.textFont, range: NSMakeRange(0, mStr.length))
+        mStr.addAttribute(.foregroundColor, value: JJToast.share.textColor, range: NSMakeRange(0, mStr.length))
+        textLabel.attributedText = mStr
+        
+        return (toastBgView, toastView, imageView, textLabel)
     }
 }
